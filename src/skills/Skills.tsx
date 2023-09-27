8cee0f49961966c605dddee198c6d71fb323f3ce
ILIABARABANOV <ilianardan@gmail.com>
2023-09-27 19:21:05 +0300

    -skills(new)

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -12,7 +12,7 @@ export const Skills = () => {
         {id: 3, title: 'TypeScript', description: 'Enhancing code quality, providing static typing and support'},
         {id: 4, title: 'CSS', description: 'Creating responsive designs, and enhancing user interfaces'},
         {id: 5, title: 'Redux', description: 'State management, implementing predictable data flow'},
-        {id: 6, title: 'API', description: 'Proficiency in connecting applications, fetching and handling data'},
+        {id: 6, title: 'API', description: 'Connecting applications, fetching and handling data'},
         {id: 7, title: 'MUI', description: 'Elegant and responsive interfaces, ensuring a consisting'},
         {id: 8, title: 'Storybook', description: 'Reusability and streamlining the development process.'},
         {id: 9, title: 'Next.js', description: 'Building performant and SEO-friendly web applications'},
